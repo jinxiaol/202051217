7d4fe8e55960c058be9b34ed4a90bb7356d80462
jinxiao <wesleywu314@gmail.com>
2025-12-17 14:39:04 +0800

    1217

---
+++ 1217/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    float jumpForce = 680.0f;
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        this.rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+    }
+}
